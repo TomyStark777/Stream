39b5bb2159e7c05334fb77888ee59e55757b5737
TomyStask777 <koumondjitimotheeklaus@gmail.com>
2024-12-31 13:51:12 +0000

    Happy New Year 2025

--- Noël/script.js
+++ Noël/script.js
@@ -7,46 +7,47 @@ let nom = document.getElementById("name");
 let afficher = document.getElementById("afficher");
 let count = 3; 
 let url = ["gdg.png","annie.jpg","billy.jpg","club.jpg","joanna.jpg"];
+
 ok.addEventListener('click', () => { 
-    
     afficher.style.display = "none"
     form.style.display = "none";
     start.style.display = "flex";
-    
 });
-
 
 start.addEventListener('click', () => {
     const interval1 = setInterval(() => { 
         start.style.fontFamily = "Times New Roman"; 
-        start.style.fontSize = "1.5em"; 
+        container.style.fontSize = "10vmin";
         if (count > 0) { 
             start.innerHTML = `<h2>${count}</h2>`; 
             count--; 
         } else if (count == 0) { 
             start.style.fontFamily = "Christmas"; 
-            start.innerHTML = `<h2>C'est Noël !</h2>`; 
+            start.innerHTML = `<h2>C'est Noël !</h2>`;
             launchConfetti(); 
             count--; 
         } else { 
-            clearInterval(interval1); 
             start.style.display = "none"; 
-            body.style.color = "skyblue"; 
-            body.style.fontSize = "2em"; 
-            body.innerHTML = `<h1><strong>${nom.value}</strong><br>Joyeux Noël !</h1>`; 
-            body.style.textShadow = "2px 2px 4px #000000;"; 
-            launchConfetti(); 
+            container.style.textShadow = "2px 2px 4px #000000;";
+            container.innerHTML = `<div id ="message"><h1><strong>${nom.value}</strong><br>Joyeux Noël !</h1></div> `;
         } 
-        }, 800);
-    },
-)
+    }, 800);
 
-// Changement de couleur du fond aléatoirement toutes les 5 secondes
-setInterval(() => {
-    body.style.backgroundImage = "url(" + url[Math.floor(Math.random() * url.length)] + ")";	
-},5000);
     
+    setInterval(() => {
+        launchConfetti();
+        container.style.fontSize = "10vmin";
+        
+        container.style.color = getRandomColor(); 
+        body.style.backgroundImage = "url(" + url[Math.floor(Math.random() * url.length)] + ")";    
+    }, 5000);
+});
 
+// Créer et ajouter l'élément audio 
+    let audio = document.createElement("audio"); 
+    audio.src = "musique.m4a"; 
+    audio.autoplay = true; 
+    body.appendChild(audio);
 
 function launchConfetti() {
     confetti({
@@ -67,8 +68,11 @@ function launchConfetti() {
     });
 }
 
-
-
-
-
-
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
